2d1c678804fef5f9cebfe144e6a9744b8f6b5d55
dougbrn <dougrbranton@gmail.com>
2015-01-17 03:10:12 -0800

    Gif making tool for pictures

--- analysis/example.py
+++ analysis/example.py
@@ -1,4 +1,34 @@
+"""
+convert -delay 5 -loop 0 *.png field.gif
+
+"""
+
+
 import numpy as np
 from matplotlib import pyplot as plt
 import pyfits
 from glob import glob
+
+path="/astro/store/scratch/tmp/bmmorris/APO/UT150108/Q1UW02/UT150117/"
+
+imagepath=glob(path+"sdss*")
+for i in range(len(imagepath)):
+    
+    image=pyfits.getdata(imagepath[i])[:,:256]
+    header=pyfits.getheader(imagepath[i])
+    n=1.25
+    m=np.mean(image)
+    s=np.std(image)
+    import matplotlib.cm as cm
+    flat=pyfits.getdata(path+'quickanalysis/flat.fits')[:,:256]
+    
+    plt.imshow(image/flat,vmin=m-n*s,vmax=m+n*s,cmap=cm.bone,origin='lower',
+               interpolation='nearest')
+    plt.title(header["DATE-OBS"].split("T")[1])
+    plt.yticks([])
+    plt.xticks([])
+    plt.savefig("plots/"+str(i).zfill(4)+".png",bbox_inches="tight")
+    plt.close()
+    
+    
+   # plt.show()
